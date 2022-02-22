5cca4fead99988815212a5963a0d14310d20c09a
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-02-22 01:05:32 -0600

    Add install tests

---
+++ tests/install_test.py
@@ -0,0 +1,53 @@
+import os
+import shutil
+import unittest
+from pathlib import Path
+from typing import Callable
+from typing import Final
+
+from fzf import FZF
+
+
+os.chdir(Path(__file__).parent)
+TARGET_DIR: Final[Path] = Path(__file__).parent / 'fzf'
+
+
+def error_handler(function: Callable, path: str, exc_info) -> None:
+    # Change Permission
+    try:
+        os.chmod(path, 0o777)
+        Path(path).unlink()
+    except Exception:
+        # Unknown Error
+        ...
+
+
+class TestCase(unittest.TestCase):
+    """Test Case"""
+
+    def tearDown(self):
+        shutil.rmtree(TARGET_DIR, onerror=error_handler)
+
+    def test_default(self):
+        FZF.install()
+        self.assertEquals(Path(TARGET_DIR).exists(), True)
+
+    def test_http(self):
+        FZF.install('http')
+        self.assertEquals(Path(TARGET_DIR).exists(), True)
+
+    def test_https(self):
+        FZF.install('https')
+        self.assertEquals(Path(TARGET_DIR).exists(), True)
+
+    def test_ssh(self):
+        FZF.install('ssh')
+        self.assertEquals(Path(TARGET_DIR).exists(), True)
+
+    def test_gh(self):
+        FZF.install('gh')
+        self.assertEquals(Path(TARGET_DIR).exists(), True)
+
+
+if __name__ == '__main__':
+    unittest.main()
