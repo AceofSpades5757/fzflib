6994a118e82338f49e582f0b937472f937384ccc
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-02-22 01:20:43 -0600

    Add methods for installation and download

--- src/fzf/main.py
+++ src/fzf/main.py
@@ -1,4 +1,5 @@
 import logging.config
+import platform
 import subprocess
 from pathlib import Path
 from typing import List
@@ -78,3 +79,69 @@ class FZF:
             return stdout.splitlines()
         else:
             return stdout.strip()
+
+    @classmethod
+    def install(cls, method=None, dry_run: bool = False) -> None:
+        """Install fzf, using git.
+
+        Will attempt to install FZF, if possible, matching the style of
+        `fzf#install()` from the Vim plugin installation.
+
+        [Documentation](https://github.com/junegunn/fzf#using-git)
+        """
+        cls.download(method=method)
+
+        if dry_run:
+            return
+        else:
+            cls._run_installer()
+
+    @classmethod
+    def _run_installer(cls) -> None:
+        """Run fzf installer."""
+
+        download_dir: Path = Path('./fzf')
+        process_options: dict = {}
+
+        if platform == 'Windows':
+            install_script: Path = download_dir / 'install.ps1'
+        else:
+            install_script = download_dir / 'install'
+
+        subprocess.run(['powershell.exe', install_script], **process_options)
+
+    @classmethod
+    def download(cls, method: str = None) -> None:
+
+        if method is None:
+            method = 'https'
+        method = method.lower()
+
+        options: List[str] = [
+            *('--depth', '1'),
+        ]
+
+        if method == 'https' or method == 'http':
+            exe: str = 'git'
+            args: list[str] = [
+                'clone',
+            ]
+            uri: str = 'https://github.com/junegunn/fzf.git'
+        elif method == 'ssh':
+            exe = 'git'
+            args = [
+                'clone',
+            ]
+            uri = 'git@github.com:junegunn/fzf.git'
+        elif method == 'gh':
+            exe = 'gh'
+            args = [
+                'repo',
+                'clone',
+            ]
+            options = []
+            uri = 'junegunn/fzf'
+
+        command: list[str] = [exe] + args + options + [uri]
+
+        subprocess.run(command)
