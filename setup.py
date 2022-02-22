daec2fe510e6025aa3e46d9530a73725dea02732
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-02-21 21:26:44 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+
+with open("README.md", "r") as fin:
+    long_description = fin.read()
+with open("LICENSE", "r") as fin:
+    license = fin.read()
+
+setuptools.setup(
+    name='fzflib',
+    version='0.1.0',
+    author='Kyle L. Davis',
+    author_email='AceofSpades5757.github@gmail.com',
+    description='A Python library for interacting with FZF.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    python_requires='>=3.6',
+    packages=setuptools.find_packages(exclude=['tests']),
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
