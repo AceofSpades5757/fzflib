9b34c8da39f31d20b5686e6be71e7d7988e1cbdc
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-02-22 00:06:04 -0600

    Update setup.py
    Add shebang, encoding, and additional pacakge details.

--- setup.py
+++ setup.py
@@ -1,27 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import setuptools
 
 
-with open("README.md", "r") as fin:
+with open('README.md', 'r') as fin:
     long_description = fin.read()
-with open("LICENSE", "r") as fin:
+with open('LICENSE', 'r') as fin:
     license = fin.read()
 
 setuptools.setup(
     name='fzflib',
     version='0.1.1',
+    license='MIT',
     author='Kyle L. Davis',
     author_email='AceofSpades5757.github@gmail.com',
     url='https://github.com/AceofSpades5757/fzflib',
     description='A Python library for interacting with FZF.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    license='MIT',
     python_requires='>=3.6',
-    packages=setuptools.find_packages('src', exclude=['tests']),
-    package_dir={'': 'src'},
+    packages=setuptools.find_packages('src'),
+    package_dir={
+        '': 'src',
+        'fzflib': 'src/fzflib',
+    },
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
