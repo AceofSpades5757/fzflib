eafd3e2de8ce8cb0ce2c72926c5f2194b847703e
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-05-23 22:36:16 -0500

    Add classifiers
    Clean setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@
 import setuptools
 
 
-with open('README.md', 'r') as fin:
+with open('README.md', 'r', encoding='utf-8') as fin:
     long_description = fin.read()
-with open('LICENSE', 'r') as fin:
-    license = fin.read()
+
 
 setuptools.setup(
     name='fzflib',
@@ -15,16 +14,22 @@ setuptools.setup(
     author='Kyle L. Davis',
     author_email='AceofSpades5757.github@gmail.com',
     url='https://github.com/AceofSpades5757/fzflib',
+    project_urls={
+        'Documentation': 'https://fzflib.readthedocs.io/en/latest/',
+        'Author': 'https://github.com/AceofSpades5757',
+    },
     description='A Python library for interacting with FZF.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     python_requires='>=3.6',
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src', 'fzflib': 'src/fzflib'},
+    test_suite="tests",
     classifiers=[
         'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
         'License :: OSI Approved :: MIT License',
+        "Development Status :: 2 - Pre-Alpha",
+        "Operating System :: OS Independent",
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
